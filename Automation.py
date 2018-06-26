8d724ad4d52598ff37149746207c577de56f24b7
DESKTOP-5A6MP0I\Filza <filza_79@yahoo.com>
2018-06-27 00:02:42 +0700

    Add assert price, change ktp number, and add scroll page

--- Automation.py
+++ Automation.py
@@ -25,6 +25,8 @@ class Automation(unittest.TestCase):
         #2 Click Quantity VVIP then buy 1 ticket
         vvip = Select(driver.find_element_by_id('ticket_1625_4905'))
         vvip.select_by_value('1')
+        time.sleep(5)
+        self.assertEqual('Rp. 50.000', driver.find_element_by_xpath('//*[@id="new_price_4905"]').text)
         driver.find_element_by_id('buy_ticket').click()
         self.assertIn('Sign In', driver.find_element_by_xpath('//*[@id="form-register"]/div/div[2]/div/a').text)
 
@@ -38,14 +40,16 @@ class Automation(unittest.TestCase):
         telephone = driver.find_element_by_xpath('//*[@id="form-register"]/div/div[4]/div[3]/input')
         telephone.send_keys('083873557966')
         ktp = driver.find_element_by_xpath('//*[@id="form-register"]/div/div[4]/div[4]/input')
-        ktp.send_keys('3276050408950005')
+        ktp.send_keys('123456789')
         wait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="form-register"]/div/div[4]/div[5]/div/div[2]/label/span'))).click()
+        driver.execute_script('window.scrollTo(0, 500)')
         driver.find_element_by_xpath('//*[@id="form-register"]/div/div[6]/div/div/button').click()
         self.assertIn('Apply', driver.find_element_by_xpath('//*[@id="form-register"]/div/div[1]/div/div[2]/button').text)
 
         #4 Choose Gopay
         gopay = driver.find_element_by_xpath('//*[@id="payment-method"]/div[2]/div[1]/div[4]/label/span')
         gopay.click()
+        driver.execute_script('window.scrollTo(0, 500)')
         wait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'btn-submit'))).click()
         self.assertIn('Pay Now', driver.find_element_by_xpath('//*[@id="submit-button"]').text)
 
@@ -66,6 +70,8 @@ class Automation(unittest.TestCase):
         #2 Click Quantity VVIP then buy 1 ticket
         vvip = Select(driver.find_element_by_id('ticket_1625_4905'))
         vvip.select_by_value('1')
+        time.sleep(5)
+        self.assertEqual('Rp. 50.000', driver.find_element_by_xpath('//*[@id="new_price_4905"]').text)
         driver.find_element_by_id('buy_ticket').click()
         self.assertIn('Sign In', driver.find_element_by_xpath('//*[@id="form-register"]/div/div[2]/div/a').text)
 
@@ -79,14 +85,16 @@ class Automation(unittest.TestCase):
         telephone = driver.find_element_by_xpath('//*[@id="form-register"]/div/div[4]/div[3]/input')
         telephone.send_keys('083873557966')
         ktp = driver.find_element_by_xpath('//*[@id="form-register"]/div/div[4]/div[4]/input')
-        ktp.send_keys('3276050408950005')
+        ktp.send_keys('123456789')
         wait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="form-register"]/div/div[4]/div[5]/div/div[2]/label/span'))).click()
+        driver.execute_script('window.scrollTo(0, 500)')
         driver.find_element_by_xpath('//*[@id="form-register"]/div/div[6]/div/div/button').click()
         self.assertIn('Apply', driver.find_element_by_xpath('//*[@id="form-register"]/div/div[1]/div/div[2]/button').text)
 
         #4 Choose Alfamart
         alfamart = driver.find_element_by_xpath('//*[@id="payment-method"]/div[2]/div[1]/div[7]/label/span')
         alfamart.click()
+        driver.execute_script('window.scrollTo(0, 500)')
         wait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'btn-submit'))).click()
         self.assertIn('Alfamart', driver.find_element_by_xpath('//*[@id="main"]/div[2]/div/div[1]/h4').text)
 
